508a685e70b20efaac771596d7a21516a578d188
Rayko <rayko.drg@gmail.com>
2022-04-03 17:06:20 -0300

    Main script is now working

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800;
+    width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   });
-  win.loadFile('index.html');
+  win.loadFile('../index.html');
 }
 
 app.on('ready', () => {
